cd30660b961f412c5db39c35bd0c2c5a7a0552c3
NamPham <nampham@nampham-Parallels-Virtual-Platform.(none)>
2013-08-08 00:14:08 +0700

    HW3-part2

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -21,8 +21,33 @@ end
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
 
-When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+Given /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split(/\s*,\s*/).each { |rating|
+    if uncheck
+      step %{I uncheck "ratings_#{rating}"}
+    else
+      step %{I check "ratings_#{rating}"}
+    end
+  }
+end
+
+# Make it easier to see all listed movies appearing on the same page
+
+Then /I should( not)? see the following movies:$/ do |should_not, movies|
+  movies.rows.each do |movie|
+    if should_not
+      step %{I should not see "#{movie[0]}"}
+    else
+      step %{I should see "#{movie[0]}"}
+    end
+  end
+end
+
+# Make it easier to see all movies in the database appearing on the same page
+
+Then /I should see all of the movies/ do
+  page.all('table#movies tbody tr').count.should == Movie.all.length
 end
